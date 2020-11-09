2af74286e55e6be800039281c94f15c04d6730fc
Noob-Jocker <74133831+Noob-Jocker@users.noreply.github.com>
2020-11-09 09:01:54 +0530

    Lol_Hanglab_Noob-Jockers

---
+++ update.sh
@@ -0,0 +1,50 @@
+Skip to content
+Noob-Jocker / hanglab
+#colour section
+red='\033[1;31m'
+rset='\033[0m'
+grn='\033[1;32m'
+ylo='\033[1;33m'
+#script coding starts
+clear
+echo " "
+echo " "
+echo '
+                  _  _  ___  ___   __  ____  ___ 
+                 ( )( )(  ,\(   \ (  )(_  _)(  _)
+                  )()(  ) _/ ) ) )/__\  )(   ) _)
+                  \__/ (_)  (___/(_)(_)(__) (___) v 1.0
+'|lolcat
+echo " "
+echo " "
+sleep 6.0
+clear
+echo -e "$red                         ChEcKiNg..>$rset"
+sleep 2.0
+clear
+echo -e "$red                         ChEcKiNg...>$rset"
+sleep 2.0
+clear
+echo -e "$red                         ChEcKiNg....>$rset"
+sleep 2.0
+clear
+echo -e "$red                         ChEcKiNg.....>$rset"
+sleep 2.0
+clear
+echo " "
+echo " "
+echo -e "$grn                updating the hanglab  please wait$rset"
+sleep 2.0
+cd $HOME
+rm -rf hanglab
+git clone https://github.com/Noob-Jocker/hanglab.git
+clear
+echo " "
+echo -e "$grn               hanglab has been updated check it now...$rset"
+sleep 3.0
+echo " "
+cd $HOME
+cd hanglab
+bash hanglab.sh
+clear
+sleep 1.0
